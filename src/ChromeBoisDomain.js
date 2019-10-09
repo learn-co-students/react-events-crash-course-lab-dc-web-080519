1deec651a19606856d5f07d70b6029c52e5a4883
github username <github email address>
2019-10-08 21:37:05 -0400

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,31 +3,37 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
-  handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
+
+  handleMouseMove = (e) => { //debugger
+    console.log('x = ' + e.clientX + '  |  ' + 'y = ' + e.clientY)
+    drawChromeBoiAtCoords(e.clientX, e.clientY)
   }
-  
-  /* TODO: Create an event handler which, when fired, invokes the provided
-   * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
-   */
-   
+
+  handleClick = (e) => toggleCycling()
+
+  handleKeyPress = (e) => {//debugger
+    // e.key === 'a' ? this.plus() : this.minus()
+    e.key === 'a' ? resize('+') : ''
+    e.key === 's' ? resize('-') : ''
+
+    }
+
+  plus = () => resize('+')
+  minus = () => resize('-')
+
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
+  /* if the key pressed was 's', then it should call `resize` with '-'
    */
-  
+
   render() {
     return (
-      <canvas 
+      <canvas
         onMouseMove={this.handleMouseMove}
+        onClick={ this.handleClick }
+        onKeyPress={ this.handleKeyPress }
         width='900'
         height='600'
         tabIndex="0">
